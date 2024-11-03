2582580106dd7ae87b2ff9e3371550e8d34ed023
Prateek Gupta <68748725+prateekgupta2411@users.noreply.github.com>
2024-11-03 23:08:38 +0530

    Add project files.

---
+++ da1/Class1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace da1
+{
+    // step 1 create an ORM
+    public class Student
+    {
+        public int SID { get; set; }
+        public string SNAME { get; set; }
+        public int DEPT { get; set; }
+
+    }
+
+    public class cdal
+    {
+        private readonly string cnnStr;
+        SqlConnection cnn;
+        SqlCommand cmd;
+
+        public cdal(string cnnStr)
+        {
+            this.cnnStr = cnnStr;
+            this.cnn = new SqlConnection(cnnStr);
+            this.cmd = new SqlCommand();
+            cmd.Connection = this.cnn;
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            List<Student> lst = new List<Student>();
+            cmd.CommandText = "Select * from Student";
+            cnn.Open();
+            SqlDataReader reader = this.cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Student std = new Student
+                {
+                    SID = (int)reader[0],
+                    SNAME = reader[1].ToString(),
+                    DEPT = (int)reader[2]
+                };
+                lst.Add(std);
+            }
+            cnn.Close();
+            return lst;
+        }
+
+        public Student GetStudentById(int id)
+        {
+            Student stu = null;
+            cmd.CommandText = $"select * from Student where SID = {id}";
+            cnn.Open();
+            SqlDataReader reader = this.cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                stu = new Student
+                {
+                    SID = (int)reader[0],
+                    SNAME = reader[1].ToString(),
+                    DEPT = (int)reader[2]
+                };
+            }
+            cnn.Close();
+            return stu;
+        }
+
+
+        public bool ModifyStudent(Student stu)
+        {
+            cmd.CommandText = $"Update Student set SNAME='{stu.SNAME}' , DEPT={stu.DEPT} where SID = {stu.SID} ";
+            cnn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            cnn.Close();
+            return rowsAffected > 0;
+        }
+
+        public bool DeleteStudent(int id)
+        {
+            cmd.CommandText = $"delete Student where SID = {id}";
+            cnn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            cnn.Close();
+            return rowsAffected > 0;
+        }
+
+
+        public bool AddStudent(Student stu)
+        {
+            cmd.CommandText = $"insert into Student values({stu.SID},'{stu.SNAME}' , {stu.DEPT})";
+            cnn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            cnn.Close();
+            return rowsAffected > 0;
+        }
+    }
+}
---
+++ mvc+dal/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using da1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_dal.Controllers
+{
+    public class HomeController : Controller
+    {
+        cdal dal = new cdal(@"Data Source = PRATEEKGUPTA\SQLEXPRESS; Initial Catalog = Prateek; Integrated Security = true; TrustServerCertificate=True;");
+        public ActionResult Index()
+        {
+            var Student = dal.GetAllStudents();
+            return View(Student);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var Student = dal.GetStudentById(id);
+            return View(Student);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Student stu)
+        {
+            dal.AddStudent(stu);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var Student = dal.GetStudentById(id);
+            return View(Student);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            dal.ModifyStudent(student);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            dal.DeleteStudent(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+
+}
